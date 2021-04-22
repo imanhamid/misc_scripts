8ba23bcce8f31347328b4b608d4ecbacb02db167
imanhamid <57411042+imanhamid@users.noreply.github.com>
2021-04-21 18:20:38 -0700

    Update parse-tped_transversions-only.R

--- parse-tped_transversions-only.R
+++ parse-tped_transversions-only.R
@@ -1,42 +1,35 @@
 #!/usr/bin/env Rscript
 suppressMessages(library(tidyverse))
 
-tped <- read.table("/Users/iman/Desktop/highfreq_all-indiv_missing0.tped", header=FALSE)
-
+tped <- read.table("HO_kopila_missing0_transversions-REDO.tped", header=FALSE)
 names(tped) <- c("chr", "pos_ID", "cM", "pos", "kpa003_1", "kpa003_2",
                  "kpa004_1", "kpa004_2", "kpa009_1", "kpa009_2", "kpa014_1", 
                  "kpa014_2", "kpa018_1", "kpa018_2")
+tped <- tped[which(tped$chr!="X" & tped$chr!="Y"),]
+pos_IDs <- read.table("../HumanOrigins/AADR/v44.3_HO_public.snp", header=FALSE)
+names(pos_IDs) <- c("rsID", "chr", "cM", "pos", "ref", "alt")
+pos_IDs <- pos_IDs[which(pos_IDs$chr!=23 & pos_IDs$chr!=24),]
+pos_IDs_transversions <- pos_IDs[which((pos_IDs$ref=="A" & pos_IDs$alt=="G") | (pos_IDs$ref=="G" & pos_IDs$alt=="A") | (pos_IDs$ref=="C" & pos_IDs$alt=="T") | (pos_IDs$ref=="T" & pos_IDs$alt=="C")),]
+pos_IDs_transversions$chr_pos <- paste0(pos_IDs_transversions$chr, "_", pos_IDs_transversions$pos)
+tped_matched <- tped[match(pos_IDs_transversions$chr_pos, tped$pos_ID),]
+tped_matched$rsID <- pos_IDs_transversions$rsID
+tped_matched$ref <- pos_IDs_transversions$ref
+tped_matched$alt <- pos_IDs_transversions$alt
+tped_matched2 <- tped_matched[which(!is.na(tped_matched$pos)),]
 
-tped2 <- tped
-tped2[fix,]$chr <- tped2[fix,]$chr-1
-tped2$pos_ID2 <- paste0(tped2$chr, "_", tped2$pos)
 
-pos_IDs <- read.table("/Users/iman/Desktop/all_variantIDs.txt", header=FALSE)
+tped_matched2$kpa003_1 <- ifelse(tped_matched2$kpa003_1==tped_matched2$ref, tped_matched2$kpa003_1, ifelse(tped_matched2$kpa003_1==tped_matched2$alt, tped_matched2$kpa003_1, 0))
+tped_matched2$kpa003_2 <- ifelse(tped_matched2$kpa003_2==tped_matched2$ref, tped_matched2$kpa003_2, ifelse(tped_matched2$kpa003_2==tped_matched2$alt, tped_matched2$kpa003_2, 0))
+tped_matched2$kpa004_1 <- ifelse(tped_matched2$kpa004_1==tped_matched2$ref, tped_matched2$kpa004_1, ifelse(tped_matched2$kpa004_1==tped_matched2$alt, tped_matched2$kpa004_1, 0))
+tped_matched2$kpa004_2 <- ifelse(tped_matched2$kpa004_2==tped_matched2$ref, tped_matched2$kpa004_2, ifelse(tped_matched2$kpa004_2==tped_matched2$alt, tped_matched2$kpa004_2, 0))
+tped_matched2$kpa009_1 <- ifelse(tped_matched2$kpa009_1==tped_matched2$ref, tped_matched2$kpa009_1, ifelse(tped_matched2$kpa009_1==tped_matched2$alt, tped_matched2$kpa009_1, 0))
+tped_matched2$kpa009_2 <- ifelse(tped_matched2$kpa009_2==tped_matched2$ref, tped_matched2$kpa009_2, ifelse(tped_matched2$kpa009_2==tped_matched2$alt, tped_matched2$kpa009_2, 0))
+tped_matched2$kpa014_1 <- ifelse(tped_matched2$kpa014_1==tped_matched2$ref, tped_matched2$kpa014_1, ifelse(tped_matched2$kpa014_1==tped_matched2$alt, tped_matched2$kpa014_1, 0))
+tped_matched2$kpa014_2 <- ifelse(tped_matched2$kpa014_2==tped_matched2$ref, tped_matched2$kpa014_2, ifelse(tped_matched2$kpa014_2==tped_matched2$alt, tped_matched2$kpa014_2, 0))
+tped_matched2$kpa018_1 <- ifelse(tped_matched2$kpa018_1==tped_matched2$ref, tped_matched2$kpa018_1, ifelse(tped_matched2$kpa018_1==tped_matched2$alt, tped_matched2$kpa018_1, 0))
+tped_matched2$kpa018_2 <- ifelse(tped_matched2$kpa018_2==tped_matched2$ref, tped_matched2$kpa018_2, ifelse(tped_matched2$kpa018_2==tped_matched2$alt, tped_matched2$kpa018_2, 0))
 
-names(pos_IDs) <- c("chr", "pos", "rsID", "REF", "ALT")
 
-pos_IDs$pos_ID <- paste0(pos_IDs$chr, "_", pos_IDs$pos)
-pos_IDs <- pos_IDs[order(pos_IDs$chr, pos_IDs$pos),]
-
-tped_matched <- tped2[match(pos_IDs$pos_ID, tped2$pos_ID2),]
-
-tped_matched$pos_ID <- pos_IDs$rsID
-
-tped_matched2 <- tped_matched
-
-tped_matched2$kpa003_1 <- ifelse(tped_matched2$kpa003_1==pos_IDs$REF, tped_matched2$kpa003_1, ifelse(tped_matched2$kpa003_1==pos_IDs$ALT, tped_matched2$kpa003_1, 0))
-tped_matched2$kpa003_2 <- ifelse(tped_matched2$kpa003_2==pos_IDs$REF, tped_matched2$kpa003_2, ifelse(tped_matched2$kpa003_2==pos_IDs$ALT, tped_matched2$kpa003_2, 0))
-tped_matched2$kpa004_1 <- ifelse(tped_matched2$kpa004_1==pos_IDs$REF, tped_matched2$kpa004_1, ifelse(tped_matched2$kpa004_1==pos_IDs$ALT, tped_matched2$kpa004_1, 0))
-tped_matched2$kpa004_2 <- ifelse(tped_matched2$kpa004_2==pos_IDs$REF, tped_matched2$kpa004_2, ifelse(tped_matched2$kpa004_2==pos_IDs$ALT, tped_matched2$kpa004_2, 0))
-tped_matched2$kpa009_1 <- ifelse(tped_matched2$kpa009_1==pos_IDs$REF, tped_matched2$kpa009_1, ifelse(tped_matched2$kpa009_1==pos_IDs$ALT, tped_matched2$kpa009_1, 0))
-tped_matched2$kpa009_2 <- ifelse(tped_matched2$kpa009_2==pos_IDs$REF, tped_matched2$kpa009_2, ifelse(tped_matched2$kpa009_2==pos_IDs$ALT, tped_matched2$kpa009_2, 0))
-tped_matched2$kpa014_1 <- ifelse(tped_matched2$kpa014_1==pos_IDs$REF, tped_matched2$kpa014_1, ifelse(tped_matched2$kpa014_1==pos_IDs$ALT, tped_matched2$kpa014_1, 0))
-tped_matched2$kpa014_2 <- ifelse(tped_matched2$kpa014_2==pos_IDs$REF, tped_matched2$kpa014_2, ifelse(tped_matched2$kpa014_2==pos_IDs$ALT, tped_matched2$kpa014_2, 0))
-tped_matched2$kpa018_1 <- ifelse(tped_matched2$kpa018_1==pos_IDs$REF, tped_matched2$kpa018_1, ifelse(tped_matched2$kpa018_1==pos_IDs$ALT, tped_matched2$kpa018_1, 0))
-tped_matched2$kpa018_2 <- ifelse(tped_matched2$kpa018_2==pos_IDs$REF, tped_matched2$kpa018_2, ifelse(tped_matched2$kpa018_2==pos_IDs$ALT, tped_matched2$kpa018_2, 0))
-
-tped_matched3 <- tped_matched2[-which(is.na(tped_matched2$pos)), -15]
-
-write.table(tped_matched3, "/Users/iman/Desktop/highfreq_all-indiv_missing0_transversions.tped", quote = FALSE, row.names = FALSE, sep = "\t")
-
-length(which(tped_matched3$kpa014_1!=0 & tped_matched3$kpa018_1!=0))
+tped_matched3 <- tped_matched2[which(tped_matched2$kpa003_1!=0 | tped_matched2$kpa004_1!=0 | tped_matched2$kpa009_1!=0 | tped_matched2$kpa014_1!=0 | tped_matched2$kpa018_1!=0),]
+tped_final <- tped_matched3[,c(1,15, 3:14)]
+write.table(tped_final, "HO_kopila_missing0_transversions_REDO.tped", append=FALSE, quote = FALSE, row.names = FALSE, sep = "\t", col.names=FALSE)
